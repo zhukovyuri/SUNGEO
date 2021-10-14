1fea4adb81d773104021122acce4aa8858e20217
zhukovyuri <zhukov.yuri@gmail.com>
2021-10-14 18:25:19 -0400

    Added pycno functionality to point2poly_krige. Bug fixes.

--- R/point2poly_krige.R
+++ R/point2poly_krige.R
@@ -7,6 +7,7 @@
 #' @param rasterz Source raster layer (or list of raster), with covariate(s) used for universal kriging. Must have identical CRS to \code{polyz}.  \code{RasterLayer} object or list of \code{RasterLayer} objects.
 #' @param yvarz Names of numeric variable(s) to be interpolated from source points layer to destination polygons. Character string or vector of character strings.
 #' @param xvarz Names of numeric variable(s) for universal Kriging, in which yvarz is linearly dependent. Character string or vector of character strings.
+#' @param pycno_yvarz  Names of spatially extensive numeric variables for which the pycnophylactic (mass-preserving) property should be preserved. Must be a subset of \code{yvarz}. Character string or vector of character strings.
 #' @param funz Aggregation function to be applied to values in \code{rasterz} and to interpolated values. Must take as an input a vector \code{x}. Default is mean.  Function.
 #' @param use_grid Use regular grid as destination layer for interpolation, before aggregating to polygons? Default is TRUE.
 #' @param nz_grid Number of grid cells in x and y direction (columns, rows). integer of length 1 or 2. Default is 100. Ignored if use_grid=FALSE.
@@ -20,7 +21,7 @@
 #'
 #' Unlike other available point-to-polygon interpolation techniques, this function currently only accepts numeric variables in \code{varz} and does not support interpolation of character strings.
 #' @import sf sp
-#' @importFrom raster extract
+#' @importFrom raster extract crs
 #' @importFrom automap autoKrige
 #' @importFrom stats as.formula aggregate
 #' @examples
@@ -67,6 +68,7 @@ point2poly_krige <- function(pointz,
                              rasterz=NULL,
                              yvarz=NULL,
                              xvarz=NULL,
+                             pycno_yvarz=NULL,
                              funz=base::mean,
                              use_grid=TRUE,
                              nz_grid=100,
@@ -74,29 +76,34 @@ point2poly_krige <- function(pointz,
                              pointz_y_coord=NULL,
                              polyz_x_coord=NULL,
                              polyz_y_coord=NULL,
-                             messagez=""){
+                             messagez=""
+){
 
   # Convert SF/DataFrame to SP
   if(class(pointz)[1] == 'sf'){
     krig_pointz <- as(pointz, "Spatial")
+    raster::crs(krig_pointz) <- paste0("+init=",tolower(sf::st_crs(pointz)$input))
   } else if(class(pointz)[1] == 'data.frame'){
     if(is.null(pointz_x_coord) == TRUE & is.null(pointz_y_coord) == TRUE){stop("Please supply both a pointz_x_coord and pointz_y_coord.")}
     colnames(pointz)[which(colnames(pointz)== pointz_x_coord)] <- "x"
     colnames(pointz)[which(colnames(pointz)== pointz_y_coord)] <- "y"
     sf.convert_pointz <- df2sf(pointz$x, pointz$y, pointz)
     krig_pointz <- as(sf.convert_pointz, "Spatial")
+    raster::crs(krig_pointz) <- "+init=epsg:4326"
   } else if(attr(class(pointz), 'package')[1] == 'sp'){
     krig_pointz <- pointz
   } else{stop("Please supply either a data frame, sf, or sp object for pointz.")}
 
   if(class(polyz)[1] == 'sf'){
     krig_polyz <- as(polyz, "Spatial")
+    raster::crs(krig_polyz) <- paste0("+init=",tolower(sf::st_crs(polyz)$input))
   } else if(class(polyz)[1] == 'data.frame'){
     if(is.null(polyz_x_coord) == TRUE & is.null(polyz_y_coord) == TRUE){stop("Please supply both a pointz_x_coord and pointz_y_coord.")}
     colnames(polyz)[which(colnames(polyz)== polyz_x_coord)] <- "x"
     colnames(polyz)[which(colnames(polyz)== polyz_y_coord)] <- "y"
     sf.convert_polyz <- df2sf(polyz$x, polyz$y, polyz)
     krig_polyz <- as(sf.convert_polyz, "Spatial")
+    raster::crs(krig_polyz) <- "+init=epsg:4326"
   } else if(attr(class(polyz), 'package') == 'sp'){
     krig_polyz <- polyz
   } else{stop("Please supply either a sf or sp object for polyz.")}
@@ -189,8 +196,25 @@ point2poly_krige <- function(pointz,
       out <- krige_result[["krige_output"]]@data[,grep("pred$|var$|stdev$",colnames(krige_result[["krige_output"]]@data))]
       return(out)
     })
+
     # bind with polygons
     krige_agg <- stats::aggregate(krige_mat,by=list(ID_kriggrid=gridz_layer$ID_kriggrid),FUN=funz, na.rm=TRUE, na.action=NULL)
+    # Pycno
+    if(!is.null(pycno_yvarz)){
+      # Loop over pycno_varz
+      for(p0 in 1:length(pycno_yvarz)){
+        # Find sum of original variable
+        sum_from <- data.table::as.data.table(krig_pointz)[,sum(get(pycno_yvarz[p0]),na.rm=TRUE)]
+        # Find matching processed variables in target geometry
+        pycno_yvarz_to <- grep(paste0("^",pycno_yvarz[p0]),names(krige_agg),value=TRUE)
+        # Rescale variables in target geometry
+        krige_agg_dt <- data.table::as.data.table(krige_agg)
+        for(p00 in 1:length(pycno_yvarz_to)){
+          krige_agg_dt[,eval(pycno_yvarz_to[p00]) := get(pycno_yvarz_to[p00])*sum_from/sum(get(pycno_yvarz_to[p00]),na.rm = TRUE)]
+        }
+        krige_agg <- as.data.frame(krige_agg_dt)
+      }
+    }
     if(class(polyz)[1] == "sf"){
       krige_out <- merge(polyz_layer, krige_agg)
       krige_out$ID_kriggrid <- NULL
